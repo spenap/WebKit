dcc78d96a7746e33323d5b13da11d067fa89f3e5
Tim Nguyen <ntim@apple.com>
2026-01-31 11:10:18 -0800

    Fix Windows build after 306568@main https://bugs.webkit.org/show_bug.cgi?id=306706 rdar://169354407
    Unreviewed build fix.
    
    * Source/WebCore/Headers.cmake:
    
    Canonical link: https://commits.webkit.org/306579@main

--- Source/WebCore/Headers.cmake
+++ Source/WebCore/Headers.cmake
@@ -1841,6 +1841,7 @@ set(WebCore_PRIVATE_FRAMEWORK_HEADERS
     layout/formattingContexts/flex/FlexRect.h
     layout/formattingContexts/flex/LogicalFlexItem.h
 
+    layout/formattingContexts/grid/FreeSpaceScenario.h
     layout/formattingContexts/grid/GridAreaLines.h
     layout/formattingContexts/grid/GridFormattingContext.h
     layout/formattingContexts/grid/GridItemRect.h
