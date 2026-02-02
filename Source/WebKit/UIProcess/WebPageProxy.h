9a3e82acf56bc97d746b85bd89b81ac5a3becd7e
Richard Robinson <richard_robinson2@apple.com>
2026-02-02 10:16:48 -0800

    Forward declare `LayerHostingContextIdentifier` in WebPageProxy instead of including it https://bugs.webkit.org/show_bug.cgi?id=306773 rdar://169448726
    Reviewed by Abrar Rahman Protyasha.
    
    Everything else is forward declared, and this should be too.
    
    * Source/WebKit/UIProcess/WebPageProxy.h:
    
    Canonical link: https://commits.webkit.org/306637@main

--- Source/WebKit/UIProcess/WebPageProxy.h
+++ Source/WebKit/UIProcess/WebPageProxy.h
@@ -29,7 +29,6 @@
 // Use forward declarations and WebPageProxyInternals.h instead.
 #include "APIObject.h"
 #include "MessageReceiver.h"
-#include <WebCore/LayerHostingContextIdentifier.h>
 #include <wtf/ApproximateTime.h>
 #include <wtf/CheckedRef.h>
 #include <wtf/CompletionHandler.h>
@@ -303,6 +302,7 @@ struct ImageBufferParameters;
 struct InheritedFrameState;
 #endif
 struct InspectorOverlayHighlight;
+struct LayerHostingContextIdentifierType;
 struct LinkIcon;
 struct LinkDecorationFilteringData;
 struct LiveRegionAnnouncementData;
@@ -394,6 +394,7 @@ using FloatBoxExtent = RectEdges<float>;
 using FrameIdentifier = ObjectIdentifier<FrameIdentifierType>;
 using IntDegrees = int32_t;
 using HTMLMediaElementIdentifier = ObjectIdentifier<MediaPlayerClientIdentifierType>;
+using LayerHostingContextIdentifier = ObjectIdentifier<LayerHostingContextIdentifierType>;
 using MediaControlsContextMenuItemID = uint64_t;
 using MediaKeySystemRequestIdentifier = ObjectIdentifier<MediaKeySystemRequestIdentifierType>;
 using MediaPlayerIdentifier = ObjectIdentifier<MediaPlayerIdentifierType>;
