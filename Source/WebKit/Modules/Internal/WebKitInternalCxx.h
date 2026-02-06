45fa4fb56ac1f23d48084c6466c7c79d66f5d0ec
Adrian Taylor <adrian_taylor@apple.com>
2026-02-06 07:54:08 -0800

    Swift WebBackForwardList (off by default) - WebKit_Internal https://bugs.webkit.org/show_bug.cgi?id=306872 rdar://169533203
    Reviewed by Richard Robinson.
    
    We're introducing a Swift version of the Back Forward List. For the general
    design and rationale, see
    https://github.com/WebKit/WebKit/pull/55393
    It will be landed as a series of separate commits in order to ease code review.
    
    This commit increases the set of header files which are considered to be part
    of the WebKit_Internal clang module. That is, the set of headers and types
    which are made available to Swift, such that Swift can use types and functions
    within these files.
    
    We are landing this commit separately in order to ease code review
    of subsequent stages. In itself it's not useful and has no effect
    until subsequent commits land.
    
    * Source/WebKit/Modules/Internal/WebKitInternalCxx.h:
    * Source/WebKit/Modules/Internal/module.modulemap:
    
    Canonical link: https://commits.webkit.org/306944@main

--- Source/WebKit/Modules/Internal/WebKitInternalCxx.h
+++ Source/WebKit/Modules/Internal/WebKitInternalCxx.h
@@ -26,4 +26,3 @@
 // Add project-level C++ header files here to be able to access them from within Swift sources.
 
 #import "IPCTesterReceiverMessages.h"
-#import "UIProcess/SwiftDemoLogoConfirmation.h"
